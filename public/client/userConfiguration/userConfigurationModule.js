34ebeb164fb9c3abd5bbe5322f5621a70152de1e
Wilco Menge <wilcomenge@hotmail.com>
2015-09-23 16:57:44 +0200

    Add user configuration controller

--- public/client/userConfiguration/userConfigurationModule.js
+++ public/client/userConfiguration/userConfigurationModule.js
@@ -6,7 +6,7 @@ userConfigurationModule.factory('UserConfiguration', ['$resource', 'API_URL', fu
         { id: '@id' },
         { update: { method: 'PUT' } });
 }]);
-
+/*
 userConfigurationModule.service( 'UserConfigurationService', [ '$rootScope', 'UserConfiguration', function($rootScope, UserConfiguration) {
 
     var service = {
@@ -26,4 +26,40 @@ userConfigurationModule.service( 'UserConfigurationService', [ '$rootScope', 'Us
 
    return service;
    
+}]);
+*/
+userConfigurationModule.controller('UserConfigurationController', ['$scope', '$modal', 'UserConfiguration', 'Notebooks', function($scope, $modal, UserConfiguration, Notebooks) {
+
+    $scope.showUserConfigurationModal = function(configuration) {
+
+    $modal.open({
+            templateUrl: 'userConfiguration/updateUserConfigurationModal.html',
+            controller: 'userConfigurationModalController',
+            resolve: {
+                configuration: ['UserConfiguration', function(UserConfiguration) {
+                    return UserConfiguration.get();
+                }],
+                notebooks: ['Notebooks', function(Notebooks) {
+                    return Notebooks.query(this.data).$promise;
+                }]
+            }
+        });
+    };
+}]);
+
+userConfigurationModule.controller('userConfigurationModalController', ['$scope', '$modalInstance', 'configuration', 'notebooks', function($scope, $modalInstance, configuration, notebooks) {
+
+    $scope.configuration = configuration;
+    $scope.notebooks = notebooks;
+
+    $scope.save = function () {
+        $scope.configuration.$update(function() {
+            $modalInstance.close();
+        });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
 }]);
