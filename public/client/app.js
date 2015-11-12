9659f9b8c937617f31d72331d7a67a19bbc4ba26
Wilco Menge <wilcomenge@hotmail.com>
2015-11-12 21:18:44 +0100

    angular moment

--- public/client/app.js
+++ public/client/app.js
@@ -1,11 +1,12 @@
-var module = angular.module('BrainDumpApp', ['ui.router', 'ngResource', 'angular-loading-bar', 'ngAnimate', 'ui.bootstrap', 'textAngular', 'angular-confirm', 'angularMoment', 'braindump.notebooks', 'braindump.notes', 'braindump.user', 'braindump.user-configuration']);
+var module = angular.module('BrainDumpApp', ['ui.router', 'ngResource', 'angular-loading-bar', 'ngAnimate', 'ui.bootstrap', 'ngSanitize', 'angular-confirm', 'zm.angular-trix-editor', 'angularMoment', 'ui.event', 'braindump.notebooks', 'braindump.notes', 'braindump.user', 'braindump.user-configuration']);
 
 module.run(function($http) {
 	$http.defaults.withCredentials = true; // allows sending of auth cookie in CORS scenario
 
+    // Set locale of moment.js to nl
     module.run(function(amMoment) {
-    amMoment.changeLocale('nl');
-});
+        amMoment.changeLocale('nl');
+    });
 });
 
 module.controller('AppController', [ '$scope', '$state', '$location', function($scope, $state, $location) {
