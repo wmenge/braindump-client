304303491a15faf08b30ee1d0e6c40ccb67204d9
Wilco Menge <wilcomenge@gmail.com>
2016-10-30 22:10:57 +0100

    Add ui-notification library (for success notifications)

--- public/client/app.js
+++ public/client/app.js
@@ -1,4 +1,4 @@
-var module = angular.module('BrainDumpApp', ['ui.router', 'ngResource', 'angular-loading-bar', 'ngAnimate', 'ui.bootstrap', 'ngSanitize', 'angular-confirm', 'angularTrix', 'angularMoment', 'ui.event', 'braindump.notebooks', 'braindump.notes', 'braindump.user', 'braindump.user-configuration']);
+var module = angular.module('BrainDumpApp', ['ui.router', 'ngResource', 'angular-loading-bar', 'ngAnimate', 'ui.bootstrap', 'ui-notification', 'ngSanitize', 'angular-confirm', 'angularTrix', 'angularMoment', 'ui.event', 'braindump.notebooks', 'braindump.notes', 'braindump.user', 'braindump.user-configuration']);
 
 module.run(function($http) {
 	$http.defaults.withCredentials = true; // allows sending of auth cookie in CORS scenario
